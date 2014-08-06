b82943c754fea9f3648f7a03145096dfa42c4071
Sung Chiu <sung.chiu@mulesoft.com>
2014-08-05 17:50:05 -0700

    Add Sphero

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 'use strict';
-var express = require('express');
-var app = express();
-var router = require('./router');
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    router = require('./router'),
+    server;
 
 
-var server = app.listen(8124,  function() {
+server = app.listen(8080,  function() {
     console.log('Listening on port %d', server.address().port);
 });
 
-router.sphero.connect();
+
 app.get('/', router.rootHandler);
-app.get('/rccar/:id/:action', router.rccarHandler);
-app.get('/pi/:id/:action', router.piHandler);
+//app.get('/rccar/:id/:action', router.rccarHandler);
+//app.get('/pi/:id/:action', router.piHandler);
 app.get('/sphero/:id/:action', router.spheroHandler);
--- router.js
+++ router.js
@@ -1,22 +1,57 @@
 'use strict';
-var rccar = require('./rccar/rccar');
-var pi = require('./pi/pi');
-var sphero = require('./sphero/sphero');
-exports.sphero = sphero;
+
+var http = require('http');
+/*
+ rccar = require('./rccar/rccar'),
+ pi = require('./pi/pi');
+ */
 
 exports.rootHandler = function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to Hackathon!');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Welcome to Hackathon!');
 };
 
-exports.rccarHandler = function(req, res) {
+
+
+exports.spheroHandler = function(req, res) {
+
+    console.log('Connection to sphero: ' + req.host + req.originalUrl);
+
+    var options = {
+            hostname: req.host,
+            port: 8352,
+            path: req.originalUrl,
+            method: 'GET'
+        },
+        data = '',
+        requestToSphero = http.request(options, function(responseFromSphero) {
+
+            responseFromSphero.setEncoding('utf8');
+
+            responseFromSphero.on('data', function(chunk){
+                data += chunk;
+            });
+
+            responseFromSphero.on('end', function () {
+
+                res.status(responseFromSphero.statusCode).send(data);
+            });
+        });
+
+    requestToSphero.on('error', function(e) {
+        res.status(404).end();
+        console.log('problem with request: ' + e.message);
+    });
+
+    requestToSphero.end();
+};
+
+
+
+/*exports.rccarHandler = function(req, res) {
 	rccar.handle(req, res);
 };
 
 exports.piHandler = function(req, res) {
 	pi.handle(req, res);
-};
-
-exports.spheroHandler = function(req, res) {
-  sphero.action(req, res);
-};
+};*/
